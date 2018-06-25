e574ff5117dc9f2a8c8166994cb19ee031ca9a87
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-06-24 22:18:42 -0300

    url

--- peladasApi/urls.py
+++ peladasApi/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token,obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-refresh/',refresh_jwt_token),
+    path('api-token-refresh/',obtain_jwt_token),
     path('auth/',include('rest_framework_social_oauth2.urls'))
 ]
