b1bd5141cba9d0bfd682e6513f6aafbe8a6e575f
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-07-03 21:24:20 -0300

    test url

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
+from players import views
 # Create your tests here.
+
+
+class ApiTestPeladas(APITestCase):
+
+
+    def test_url(self):
+        url = reverse('api/peladas')
+        response = self.client.get(url, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(len(response.data), 1)
+
+
+class ApitUserPeladas(APITestCase):
+
+    def test_url(self):
+        url = reverse('api/user-peladas')
+        response = self.client.get(url, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(len(response.data), 1)
+
